da35b61a59d91641fa7666e92643560fe4f4b648
Lino004 <h.ayinlamouhayad@gmail.com>
2023-05-20 19:40:45 +0000

    Closing tab

--- src/Samples/WorkItemFormPage/SimpleTagPickerExample.tsx
+++ src/Samples/WorkItemFormPage/SimpleTagPickerExample.tsx
@@ -1,87 +1,64 @@
+import { WorkItemField } from "azure-devops-extension-api/WorkItemTracking";
 import { useObservableArray } from "azure-devops-ui/Core/Observable";
 import { ISuggestionItemProps } from "azure-devops-ui/SuggestionsList";
 import { TagPicker } from "azure-devops-ui/TagPicker";
 import * as React from "react";
-import "azure-devops-ui/Core/override.css";
 
-interface TagItem {
-    id: number;
-    text: string;
+interface ComponentProps {
+  data: WorkItemField[],
+  setDataSelected(data: WorkItemField[]): void
 }
 
-const tagData: TagItem[] = [
-    {
-        id: 1,
-        text: "Item 1"
-    },
-    {
-        id: 2,
-        text: "Item 2"
-    },
-    {
-        id: 3,
-        text: "Item 3"
-    }
-];
+export const SimpleTagPickerExample: React.FunctionComponent<ComponentProps> = ({ data, setDataSelected }) => {
+  const [tagItems, setTagItems] = useObservableArray<WorkItemField>([]);
+  const [suggestions, setSuggestions] = useObservableArray<WorkItemField>([]);
 
-export const SimpleTagPickerExample: React.FunctionComponent<{}> = () => {
-    const [tagItems, setTagItems] = useObservableArray<TagItem>(tagData.slice(0, 2));
-    const [suggestions, setSuggestions] = useObservableArray<TagItem>([]);
+  const areTagsEqual = (a: WorkItemField, b: WorkItemField) => {
+    return a.referenceName === b.referenceName;
+  };
 
-    const areTagsEqual = (a: TagItem, b: TagItem) => {
-        return a.id === b.id;
+  const convertItemToPill = (tag: WorkItemField) => {
+    return {
+      content: tag.name,
+      onClick: () => alert(`Clicked tag "${tag.name}"`)
     };
+  };
 
-    const convertItemToPill = (tag: TagItem) => {
-        return {
-            content: tag.text,
-            onClick: () => alert(`Clicked tag "${tag.text}"`)
-        };
-    };
-
-    const onSearchChanged = (searchValue: string) => {
-        setSuggestions(
-            tagData
-                .filter(
-                    // Items not already included
-                    testItem =>
-                        tagItems.value.findIndex(
-                            testSuggestion => testSuggestion.id == testItem.id
-                        ) === -1
-                )
-                .filter(
-                    testItem => testItem.text.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
-                )
-        );
-    };
-
-    const onTagAdded = (tag: TagItem) => {
-        setTagItems([...tagItems.value, tag]);
-    };
-
-    const onTagRemoved = (tag: TagItem) => {
-        setTagItems(tagItems.value.filter(x => x.id !== tag.id));
-    };
-
-    const renderSuggestionItem = (tag: ISuggestionItemProps<TagItem>) => {
-        return <div className="body-m">{tag.item.text}</div>;
-    };
-
-    return (
-        <div className="flex-column">
-            <TagPicker
-                areTagsEqual={areTagsEqual}
-                convertItemToPill={convertItemToPill}
-                noResultsFoundText={"No results found"}
-                onSearchChanged={onSearchChanged}
-                onTagAdded={onTagAdded}
-                onTagRemoved={onTagRemoved}
-                renderSuggestionItem={renderSuggestionItem}
-                selectedTags={tagItems}
-                suggestions={suggestions}
-                suggestionsLoading={false}
-                ariaLabel={"Search for additional tags"}
-            />
-        </div>
+  const onSearchChanged = (searchValue: string) => {
+    setSuggestions(data
+      .filter(testItem => testItem.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
     );
+  };
+
+  const onTagAdded = (tag: WorkItemField) => {
+    setTagItems([...tagItems.value, tag]);
+    setDataSelected(tagItems.value)
+  };
+
+  const onTagRemoved = (tag: WorkItemField) => {
+    setTagItems(tagItems.value.filter(x => x.referenceName !== tag.referenceName));
+  };
+
+  const renderSuggestionItem = (tag: ISuggestionItemProps<WorkItemField>) => {
+    return <div className="body-m">{tag.item.name}</div>;
+  };
+
+  return (
+    <div className="flex-column">
+      <label>Document Closing List</label>
+      <TagPicker
+        areTagsEqual={areTagsEqual}
+        convertItemToPill={convertItemToPill}
+        noResultsFoundText={"No results found"}
+        onSearchChanged={onSearchChanged}
+        onTagAdded={onTagAdded}
+        onTagRemoved={onTagRemoved}
+        renderSuggestionItem={renderSuggestionItem}
+        selectedTags={tagItems}
+        suggestions={suggestions}
+        suggestionsLoading={false}
+        ariaLabel={"Search..."}
+      />
+    </div>
+  );
 };
--- src/Samples/WorkItemFormPage/WorkItemFormPage.tsx
+++ src/Samples/WorkItemFormPage/WorkItemFormPage.tsx
@@ -1,128 +1,54 @@
 import {
-  IWorkItemChangedArgs,
-  IWorkItemFieldChangedArgs,
   IWorkItemFormService,
-  IWorkItemLoadedArgs,
   WorkItemField,
   WorkItemTrackingServiceIds
 } from "azure-devops-extension-api/WorkItemTracking";
 import * as SDK from "azure-devops-extension-sdk";
-import { Button } from "azure-devops-ui/Button";
 import * as React from "react";
 import { showRootComponent } from "../../Common";
-import "azure-devops-ui/Core/override.css";
+import "./iconFont.css";
 import { SimpleTagPickerExample } from "./SimpleTagPickerExample";
 
 interface WorkItemFormPageComponentState {
-  eventContent: string;
+  fieldsSelected: WorkItemField[];
   fields: WorkItemField[];
 }
-interface TagItem {
-  id: number;
-  text: string;
-}
-
-const tagData: TagItem[] = [
-  {
-      id: 1,
-      text: "Item 1"
-  },
-  {
-      id: 2,
-      text: "Item 2"
-  },
-  {
-      id: 3,
-      text: "Item 3"
-  }
-];
 
 export class WorkItemFormPageComponent extends React.Component<{},  WorkItemFormPageComponentState> {
 
   constructor(props: {}) {
     super(props);
     this.state = {
-      eventContent: "",
+      fieldsSelected: [],
       fields: [],
     };
   }
 
   public componentDidMount() {
     SDK.init().then(async () => {
-      this.registerEvents();
       this.getFields();
     });
   }
 
   public render(): JSX.Element {
     return (
-      <div>
-        <Button
-          className="sample-work-item-button"
-          text="Click me to change title!"
-          onClick={() => this.onClick()}
+      <div className="margin-8">
+        <SimpleTagPickerExample
+          data={this.state.fields}
+          setDataSelected={(data) => {
+            this.setState({fieldsSelected: data})
+          }}
         />
-        <div className="sample-work-item-events">{this.state.eventContent}</div>
-        <SimpleTagPickerExample />
+        <div className="flex-column rhythm-vertical-4">
+          {this.state.fieldsSelected.map(field => {
+            return (
+              <span>
+                {field.name}
+              </span>
+            )
+          })}
+        </div>
       </div>
-    );
-  }
-
-  private registerEvents() {
-    SDK.register(SDK.getContributionId(), () => {
-      return {
-        // Called when the active work item is modified
-        onFieldChanged: (args: IWorkItemFieldChangedArgs) => {
-          this.setState({
-            eventContent: `onFieldChanged - ${JSON.stringify(args)}`
-          });
-        },
-
-        // Called when a new work item is being loaded in the UI
-        onLoaded: (args: IWorkItemLoadedArgs) => {
-          this.setState({
-            eventContent: `onLoaded - ${JSON.stringify(args)}`
-          });
-        },
-
-        // Called when the active work item is being unloaded in the UI
-        onUnloaded: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onUnloaded - ${JSON.stringify(args)}`
-          });
-        },
-
-        // Called after the work item has been saved
-        onSaved: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onSaved - ${JSON.stringify(args)}`
-          });
-        },
-
-        // Called when the work item is reset to its unmodified state (undo)
-        onReset: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onReset - ${JSON.stringify(args)}`
-          });
-        },
-
-        // Called when the work item has been refreshed from the server
-        onRefreshed: (args: IWorkItemChangedArgs) => {
-          this.setState({
-            eventContent: `onRefreshed - ${JSON.stringify(args)}`
-          });
-        }
-      };
-    });
-  }
-
-  private async onClick() {
-    const workItemFormService = await SDK.getService<IWorkItemFormService>(
-      WorkItemTrackingServiceIds.WorkItemFormService
-    );
-    workItemFormService.setFieldValue(
-      "System.Title",
-      "Title set from your group extension!"
     );
   }
 
